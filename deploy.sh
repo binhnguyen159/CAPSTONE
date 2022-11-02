615c8c2e8b3d804f05251321fcbfa0f82a610260
binhnguyen159 <mr.peace19999@gmail.com>
2022-11-02 21:08:50 +0700

    test

--- deploy.sh
+++
@@ -1,5 +0,0 @@
-cd .circleci/files
-aws cloudformation deploy \
-         --template-file cloudfront.yml \
-         --stack-name InitialStack\
-         --parameter-overrides WorkflowID=udapeople-kk1j287dhjppmz437
